5196556f8c0cd99be6e58a5bba60a4942d3e9a5d
Han Mei <js7122420@gmail.com>
2025-02-28 17:17:50 +0900

    프로젝트 세팅팅

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Test from "./pages/Test";
+import Home from "./pages/Home";
+
+function App() {
+	return (
+		<>
+			{/* <Test></Test> */}
+			<Home></Home>
+		</>
+	);
+}
+
+export default App;
