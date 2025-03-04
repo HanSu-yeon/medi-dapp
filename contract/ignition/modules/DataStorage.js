1d0771323220891610842a79bceae0c4286e8e9a
Han Mei <js7122420@gmail.com>
2025-03-04 17:58:14 +0900

    [del] 파일 삭제

--- contract/ignition/modules/DataStorage.js
+++
@@ -1,7 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("DataStorageModule", (m) => {
-	const dataStorage = m.contract("DataStorage");
-
-	return { dataStorage };
-});
