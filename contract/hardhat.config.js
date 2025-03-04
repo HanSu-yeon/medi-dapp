a2ab72430b8bd8f0dcba5891c5e984758393e8dc
Han Mei <js7122420@gmail.com>
2025-03-04 18:00:12 +0900

    환경세팅팅

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -6,6 +7,11 @@ module.exports = {
 
 	networks: {
 		hardhat: {},
+		kairos: {
+			url: process.env.KAIROS_TESTNET_URL || "",
+			gasPrice: 250000000000,
+			accounts: process.env.KAIROS_PRIVATE_KEY !== undefined ? [process.env.KAIROS_PRIVATE_KEY] : [],
+		},
 	},
 
 	solidity: "0.8.0",
